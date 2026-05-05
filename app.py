687210f4c1d69b5f1813bfb2e1ea94fe8b5e0044
Hasham Aziz <hashambangyal@gmail.com>
2026-05-05 18:16:43 +0500

    fixed deployment issues in app.py

--- app.py
+++ app.py
@@ -1,35 +1,36 @@
-from flask import Flask , request , jsonify
+from flask import Flask, request, jsonify
 import joblib 
 import pandas as pd
 import numpy as np
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-#  loading the model and columns 
+# Load model and columns
 model = joblib.load('houe_price_predict.pkl')
 columns = joblib.load('column.pkl')
 
 @app.route('/')
 def home():
-    return " House Prediction API running "
+    return "House Prediction API running"
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
 
     df = pd.DataFrame([data])
-
-    df = df.reindex(columns=columns , fill_value=0)
+    df = df.reindex(columns=columns, fill_value=0)
 
     pred = model.predict(df)
-    price =  np.expm1(pred)
-    print(price)
+    price = np.expm1(pred)
 
     return jsonify({
-        "Predictiedprice": float(price[0])
+        "PredictedPrice": float(price[0])
     })
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
